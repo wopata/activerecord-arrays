d3cd0307ceb7e70b366aa36f9e16d07090df2163
Roland Venesz <roland.venesz@wopata.com>
2012-03-29 12:01:26 +0200

    Table definition accepts multiple columns

--- activerecord-arrays.gemspec
+++ activerecord-arrays.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = 'activerecord-arrays'
-  s.version = '0.1.6'
+  s.version = '0.1.7'
 
   s.summary = 'Arrays for ActiveRecord'
   s.description = 'Arrays for ActiveRecord'
 
   s.required_rubygems_version = Gem::Requirement.new(">= 1.3.5")
   s.authors = ['Roland Venesz']
-  s.date = '2012-03-06'
+  s.date = '2012-03-29'
   s.files = %w(README.md LICENSE) + Dir['lib/**/*.rb']
   s.require_paths = %w(lib)
 
--- lib/activerecord/arrays/table_definition.rb
+++ lib/activerecord/arrays/table_definition.rb
@@ -1,8 +1,12 @@
 module ActiveRecord::Arrays::TableDefinition
-  for t in %w(string integer float decimal) do
-    module_eval "
-      def #{t}_array name, *rest
-        column name, :#{t}_array, *rest
-      end", __FILE__, __LINE__
+  for t in %w(string integer float decimal)
+    module_eval "def #{t}_array *args; array_column '#{t}', *args; end", __FILE__, __LINE__
+  end
+
+  protected
+
+  def array_column type, *rest
+    type, opts = "#{type}_array", rest.extract_options!
+    rest.each { |name| column name, type, opts }
   end
 end
